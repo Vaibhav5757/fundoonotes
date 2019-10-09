a19c6852ca098645ff0aba287ed8733b437bae3e
mobicomp <vaibhavpratihar1771@gmail.com>
2019-10-09 18:21:08 +0530

    Added UI part for search bar in main content

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-components/dashboard',
@@ -16,6 +18,16 @@ export class DashboardComponent {
       map(result => result.matches)
     );
     
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private titleService: Title,private breakpointObserver: BreakpointObserver,) {
+    this.setTitle('Dashboard');
+  }
+
+  changeHide(){
+    this.hide = !this.hide;
+  }
   
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
+
   }
